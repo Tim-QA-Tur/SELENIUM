91127aaad85841bd889e526184c73ee79c9764ba
Timur <5709037@mail.ru>
2025-08-19 11:43:48 +0700

    Update test_pokemons.py

--- pytest/api/test_pokemons.py
+++ pytest/api/test_pokemons.py
@@ -17,10 +17,11 @@ def test_pokemon_name():
 
 def test_trainer_name():
     response_get_name = requests.get(url = f'{URL}/me', headers=HEADER)
-    assert response_get_name.json()["data"][0]["trainer_name"] == "Барбос" # имя тренера
+    assert response_get_name.json()["data"][0]["trainer_name"] == "Барбос" # имя вашего тренера
 
 @pytest.mark.parametrize('key, value', [('name', 'Chuichui'), ('trainer_id', TRAINER_ID), ('id', '288391')])
 def test_parametrize(key, value):
     response_parametrize = requests.get(url = f'{URL}/pokemons', params = {'trainer_id' : TRAINER_ID})
     assert response_parametrize.json()["data"][0][key] == value
+
 
