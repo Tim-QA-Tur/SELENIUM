1d54a880b13f5c68b917849a24616cb3d0bee650
Timur <5709037@mail.ru>
2025-08-19 11:09:21 +0700

    Update test_pokemons.py

--- pytest/api/test_pokemons.py
+++ pytest/api/test_pokemons.py
@@ -2,9 +2,9 @@ import requests
 import pytest
 
 URL = 'https://api.pokemonbattle.ru/v2'
-TOKEN = 'cf05f8742a6e0c238c8704d40014512d'
+TOKEN = 'введите свой токен'
 HEADER = {'Content-Type':'application/json', 'trainer_token' :TOKEN}
-TRAINER_ID = '28363'
+TRAINER_ID = 'введите свой трейнер ID'
 
 def test_status_code():
     response = requests.get(url = f'{URL}/pokemons', params = {'trainer_id' :TRAINER_ID})
@@ -13,13 +13,14 @@ def test_status_code():
 
 def test_pokemon_name():
     response_get_pok_name = requests.get(url = f'{URL}/pokemons', params = {'trainer_id' :TRAINER_ID})
-    assert response_get_pok_name.json()["data"][0]["name"] == 'Chuichui'
+    assert response_get_pok_name.json()["data"][0]["name"] == 'Chuichui' # имя покемона
 
 def test_trainer_name():
     response_get_name = requests.get(url = f'{URL}/me', headers=HEADER)
-    assert response_get_name.json()["data"][0]["trainer_name"] == "Барбос"
+    assert response_get_name.json()["data"][0]["trainer_name"] == "Барбос" # имя тренера
 
 @pytest.mark.parametrize('key, value', [('name', 'Chuichui'), ('trainer_id', TRAINER_ID), ('id', '288391')])
 def test_parametrize(key, value):
     response_parametrize = requests.get(url = f'{URL}/pokemons', params = {'trainer_id' : TRAINER_ID})
     assert response_parametrize.json()["data"][0][key] == value
+
